443c07e7a4131b4f99fb3cd42d44e52a6cb33bb4
Mikhail <mikushnerev@yandex.ru>
2022-03-17 22:53:13 +0300

    Изменил management.py + gitignore пополнил

--- table_management/table/management.py
+++ table_management/table/management.py
@@ -1,20 +1,34 @@
-# import csv, sqlite3
+import csv, sqlite3
 
-# # from .models import Category
 
-# con = sqlite3.connect("db.sqlite3")
-# cur = con.cursor()
-# # cur.execute("CREATE TABLE content_category (id, name, slug);")
+con = sqlite3.connect("db.sqlite3")
+cur = con.cursor()
+cur.execute("CREATE TABLE ttable (id, RollNo, Class, First_Name, Last_Name);")
 
-# # path = "./category.csv"
+path = "D:/Final/Export-Write-CSV/table_management/static/data/table.csv"
 
-# with open(
-#     './category.csv',
-#     'r',
-#     encoding='utf-8'
-# ) as csvfiles:
-#     reader = csv.DictReader(csvfiles)
-#     created = [(i['id'], i['name'], i['slug']) if i else 1 for i in reader]
-# cur.executemany("INSERT INTO rewiews_categories (id, name, slug) VALUES (?, ?, ?);", created)
-# con.commit()
-# con.close()
+with open(
+    path,
+    'r',
+    encoding='utf-8'
+) as csvfiles:
+    reader = csv.DictReader(
+        csvfiles,
+        delimiter=';',
+        quotechar=','
+    )
+    created = [
+        (
+            i['RollNo'],
+            i['id'],
+            i['Class'],
+            i['First_Name'],
+            i['Last_Name']
+        ) if i else 1 for i in reader
+    ]
+cur.executemany(
+    "INSERT INTO ttable (RollNo, id, Class, First_Name, Last_Name) VALUES (?, ?, ?, ?, ?);",
+    created
+)
+con.commit()
+con.close()
